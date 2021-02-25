c3c13ea87370bedf5755795aa17c2a710db83df2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-25 16:27:08 +0100

    63. DOM: Atributos y  Data-Attributes

--- dom.js
+++ dom.js
@@ -29,17 +29,49 @@
 
 // 62. DOM: Nodos, Elmentos y Selectores
 
-console.log(document.getElementsByTagName('li'))
-console.log(document.getElementsByClassName('card'))
-console.log(document.getElementsByName('nombre'))
-console.log(document.getElementById('menu'))
-console.log(document.querySelector('#menu'))
-console.log(document.querySelector('a'))
-console.log(document.querySelectorAll('a'))
-console.log(document.querySelectorAll('a').length)
-document.querySelectorAll('a').forEach(el => console.log(el))
-console.log(document.querySelector('.card'))
-console.log(document.querySelectorAll('.card'))
-console.log(document.querySelectorAll('.card')[2])
-console.log(document.querySelector('#menu li'))
-console.log(document.querySelectorAll('#menu li'))
+// console.log(document.getElementsByTagName('li'))
+// console.log(document.getElementsByClassName('card'))
+// console.log(document.getElementsByName('nombre'))
+// console.log(document.getElementById('menu'))
+// console.log(document.querySelector('#menu'))
+// console.log(document.querySelector('a'))
+// console.log(document.querySelectorAll('a'))
+// console.log(document.querySelectorAll('a').length)
+// document.querySelectorAll('a').forEach(el => console.log(el))
+// console.log(document.querySelector('.card'))
+// console.log(document.querySelectorAll('.card'))
+// console.log(document.querySelectorAll('.card')[2])
+// console.log(document.querySelector('#menu li'))
+// console.log(document.querySelectorAll('#menu li'))
+
+// 63. DOM: Atributos y Data-Attributes
+
+console.log(document.documentElement.lang)
+console.log(document.documentElement.getAttribute('lang'))
+console.log(document.querySelector('.link-dom').href)
+console.log(document.querySelector('.link-dom').getAttribute('href'))
+
+document.documentElement.lang = 'es'
+console.log(document.documentElement.lang)
+document.documentElement.setAttribute('lang', 'es-MX')
+console.log(document.documentElement.lang)
+
+const $linkDOM = document.querySelector('.link-dom')
+
+$linkDOM.setAttribute('target', '_blank')
+$linkDOM.setAttribute('rel', 'noopener')
+$linkDOM.setAttribute('href', 'https://youtube.com/jonmircha')
+console.log($linkDOM.hasAttribute('rel'))
+$linkDOM.removeAttribute('rel')
+console.log($linkDOM.hasAttribute('rel'))
+
+// Data-Attributes
+console.log($linkDOM.getAttribute('data-description'))
+console.log($linkDOM.dataset)
+console.log($linkDOM.dataset.description)
+$linkDOM.setAttribute('data-description', 'Modelo de Objeto del Documento')
+console.log($linkDOM.dataset.description)
+$linkDOM.dataset.description = 'Suscríbete a mi canal y comparte'
+console.log($linkDOM.hasAttribute('data-id'))
+console.log($linkDOM.removeAttribute('data-id'))
+console.log($linkDOM.hasAttribute('data-id'))
