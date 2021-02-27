cef15d63470583b972f15ff3f30353e2ab25ff76
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-27 12:42:35 +0100

    69. DOM: Templates HTML

--- dom.js
+++ dom.js
@@ -56,72 +56,111 @@
 // console.log($cards.children[3].closest('section'))
 
 // 68. DOM: Creando Elementos y Fragmentos
-const $figure = document.createElement('figure'),
-	$img = document.createElement('img'),
-	$figcaption = document.createElement('figcaption'),
-	$figcaptionText = document.createTextNode('Animals')
-$cards = document.querySelector('.cards')
-$figure2 = document.createElement('figure')
+// const $figure = document.createElement('figure'),
+// 	$img = document.createElement('img'),
+// 	$figcaption = document.createElement('figcaption'),
+// 	$figcaptionText = document.createTextNode('Animals')
+// $cards = document.querySelector('.cards')
+// $figure2 = document.createElement('figure')
 
-$img.setAttribute('src', 'https://placeimg.com/200/200/animals')
-$img.setAttribute('alt', 'Animals')
-$figure.classList.add('card')
+// $img.setAttribute('src', 'https://placeimg.com/200/200/animals')
+// $img.setAttribute('alt', 'Animals')
+// $figure.classList.add('card')
 
-$figcaption.appendChild($figcaptionText)
-$figure.appendChild($img)
-$figure.appendChild($figcaption)
-$cards.appendChild($figure)
+// $figcaption.appendChild($figcaptionText)
+// $figure.appendChild($img)
+// $figure.appendChild($figcaption)
+// $cards.appendChild($figure)
 
-$figure2.innerHTML = `
-<img src="https://placeimg.com/200/200/people" alt="People">
-<figcaption>People</figcaption>
-`
+// $figure2.innerHTML = `
+// <img src="https://placeimg.com/200/200/people" alt="People">
+// <figcaption>People</figcaption>
+// `
 
-$figure2.classList.add('card')
-$cards.appendChild($figure2)
+// $figure2.classList.add('card')
+// $cards.appendChild($figure2)
 
-const estaciones = ['Primavera', 'Verano', 'Otoño', 'Invierno'],
-	$ul = document.createElement('ul')
+// const estaciones = ['Primavera', 'Verano', 'Otoño', 'Invierno'],
+// 	$ul = document.createElement('ul')
 
-document.write('<h3>Estaciones del año</h3>')
-document.body.appendChild($ul)
+// document.write('<h3>Estaciones del año</h3>')
+// document.body.appendChild($ul)
 
-estaciones.forEach(el => {
-	const $li = document.createElement('li')
-	$li.textContent = el
-	$ul.appendChild($li)
-})
+// estaciones.forEach(el => {
+// 	const $li = document.createElement('li')
+// 	$li.textContent = el
+// 	$ul.appendChild($li)
+// })
 
-const continentes = ['África', 'América', 'Asia', 'Europa', 'Oceanía'],
-	$ul2 = document.createElement('ul')
+// const continentes = ['África', 'América', 'Asia', 'Europa', 'Oceanía'],
+// 	$ul2 = document.createElement('ul')
 
-document.write('<h3>Continentes del Mundo</h3>')
-document.body.appendChild($ul2)
-$ul2.innerHTML = ''
-continentes.forEach(el => ($ul2.innerHTML += `<li>${el}</li>`))
+// document.write('<h3>Continentes del Mundo</h3>')
+// document.body.appendChild($ul2)
+// $ul2.innerHTML = ''
+// continentes.forEach(el => ($ul2.innerHTML += `<li>${el}</li>`))
 
-const meses = [
-		'Enero',
-		'Febrero',
-		'Marzo',
-		'Abril',
-		'Mayo',
-		'Junio',
-		'Julio',
-		'Septiembre',
-		'Octubre',
-		'Noviembre',
-		'Diciembre',
-	],
-	$ul3 = document.createElement('ul'),
+// const meses = [
+// 		'Enero',
+// 		'Febrero',
+// 		'Marzo',
+// 		'Abril',
+// 		'Mayo',
+// 		'Junio',
+// 		'Julio',
+// 		'Septiembre',
+// 		'Octubre',
+// 		'Noviembre',
+// 		'Diciembre',
+// 	],
+// 	$ul3 = document.createElement('ul'),
+// 	$fragment = document.createDocumentFragment()
+
+// meses.forEach(el => {
+// 	const $li = document.createElement('li')
+// 	$li.textContent = el
+// 	$fragment.appendChild($li)
+// })
+
+// document.write('<h3>Meses del Año</h3>')
+// $ul3.appendChild($fragment)
+// document.body.appendChild($ul3)
+
+// 69. DOM: Templates HTML
+
+const $cards = document.querySelector('.cards'),
+	$template = document.getElementById('template-card').content,
 	$fragment = document.createDocumentFragment()
+cardContent = [
+	{
+		title: 'Tecnología',
+		img: 'https://placeimg.com/200/200/tech',
+	},
+	{
+		title: 'Animales',
+		img: 'https://placeimg.com/200/200/animals',
+	},
+	{
+		title: 'Arquitectura',
+		img: 'https://placeimg.com/200/200/arch',
+	},
+	{
+		title: 'Gente',
+		img: 'https://placeimg.com/200/200/people',
+	},
+	{
+		title: 'Naturaleza',
+		img: 'https://placeimg.com/200/200/nature',
+	},
+]
 
-meses.forEach(el => {
-	const $li = document.createElement('li')
-	$li.textContent = el
-	$fragment.appendChild($li)
+cardContent.forEach(el => {
+	$template.querySelector('img').setAttribute('src', el.img)
+	$template.querySelector('img').setAttribute('alt', el.title)
+	$template.querySelector('figcaption').textContent = el.title
+
+	let $clone = document.importNode($template, true)
+	$fragment.appendChild($clone)
 })
 
-document.write('<h3>Meses del Año</h3>')
-$ul3.appendChild($fragment)
-document.body.appendChild($ul3)
+$cards.appendChild($fragment)
