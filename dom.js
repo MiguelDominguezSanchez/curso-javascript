28ef3e8446337771acc2b4d5e84feb5c66134c49
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-27 13:30:29 +0100

    70. DOM: Modification Elementos (Old Style)

--- dom.js
+++ dom.js
@@ -128,39 +128,56 @@
 
 // 69. DOM: Templates HTML
 
+// const $cards = document.querySelector('.cards'),
+// 	$template = document.getElementById('template-card').content,
+// 	$fragment = document.createDocumentFragment()
+// cardContent = [
+// 	{
+// 		title: 'Tecnología',
+// 		img: 'https://placeimg.com/200/200/tech',
+// 	},
+// 	{
+// 		title: 'Animales',
+// 		img: 'https://placeimg.com/200/200/animals',
+// 	},
+// 	{
+// 		title: 'Arquitectura',
+// 		img: 'https://placeimg.com/200/200/arch',
+// 	},
+// 	{
+// 		title: 'Gente',
+// 		img: 'https://placeimg.com/200/200/people',
+// 	},
+// 	{
+// 		title: 'Naturaleza',
+// 		img: 'https://placeimg.com/200/200/nature',
+// 	},
+// ]
+
+// cardContent.forEach(el => {
+// 	$template.querySelector('img').setAttribute('src', el.img)
+// 	$template.querySelector('img').setAttribute('alt', el.title)
+// 	$template.querySelector('figcaption').textContent = el.title
+
+// 	let $clone = document.importNode($template, true)
+// 	$fragment.appendChild($clone)
+// })
+
+// $cards.appendChild($fragment)
+
+// 70. DOM: Modificando Elementos (Old Style)
+
 const $cards = document.querySelector('.cards'),
-	$template = document.getElementById('template-card').content,
-	$fragment = document.createDocumentFragment()
-cardContent = [
-	{
-		title: 'Tecnología',
-		img: 'https://placeimg.com/200/200/tech',
-	},
-	{
-		title: 'Animales',
-		img: 'https://placeimg.com/200/200/animals',
-	},
-	{
-		title: 'Arquitectura',
-		img: 'https://placeimg.com/200/200/arch',
-	},
-	{
-		title: 'Gente',
-		img: 'https://placeimg.com/200/200/people',
-	},
-	{
-		title: 'Naturaleza',
-		img: 'https://placeimg.com/200/200/nature',
-	},
-]
+	$newCard = document.createElement('figure'),
+	$cloneCards = $cards.cloneNode(true)
 
-cardContent.forEach(el => {
-	$template.querySelector('img').setAttribute('src', el.img)
-	$template.querySelector('img').setAttribute('alt', el.title)
-	$template.querySelector('figcaption').textContent = el.title
+$newCard.innerHTML = `
+<img src="http://placeimg.com/200/200/any" alt="Any">
+<figcaption>Any</figcaption>
+`
+$newCard.classList.add('card')
 
-	let $clone = document.importNode($template, true)
-	$fragment.appendChild($clone)
-})
-
-$cards.appendChild($fragment)
+// $cards.replaceChild($newCard, $cards.children[2])
+$cards.removeChild($cards.lastElementChild)
+// $cards.insertBefore($newCard, $cards.firstElementChild)
+document.body.appendChild($cloneCards)
