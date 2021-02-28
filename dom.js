53b0453b0b567d17a29ad142cff75bcaefcffe02
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-28 16:40:26 +0100

    76. DOM: Delegación de Eventos

--- dom.js
+++ dom.js
@@ -295,29 +295,51 @@ const $divsEventos = document.querySelectorAll('.eventos-flujo div')
 $linkEventos = document.querySelector('.eventos-flujo a')
 
 function flujoEventos(e) {
+	// console.log(
+	// 	`Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
+	// )
 	console.log(
-		`Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
+		`Hola te saluda ${this}, el click lo originó ${e.target.className}`
 	)
-	e.stopPropagation()
+	// e.stopPropagation()
 }
 
 console.log($divsEventos)
 
-$divsEventos.forEach(div => {
-	// Fase de burbúja
-	div.addEventListener('click', flujoEventos)
-	// div.addEventListener('click', flujoEventos, false)
-	// Fase de captura
-	// div.addEventListener('click', flujoEventos, true)
-	// div.addEventListener('click', flujoEventos, {
-	// 	capture: false,
-	// 	once: true,
-	// })
-})
+// $divsEventos.forEach(div => {
+// 	// Fase de burbúja
+// 	div.addEventListener('click', flujoEventos)
+// 	// div.addEventListener('click', flujoEventos, false)
+// 	// Fase de captura
+// 	// div.addEventListener('click', flujoEventos, true)
+// 	// div.addEventListener('click', flujoEventos, {
+// 	// 	capture: false,
+// 	// 	once: true,
+// 	// })
+// })
 
 // 75. DOM: StopPropagation & preventDefault
 
-$linkEventos.addEventListener('click', e => {
-	alert('Hola soy tu amigo y docente digital...Jonathan Mircha')
-	e.preventDefault()
+// $linkEventos.addEventListener('click', e => {
+// 	alert('Hola soy tu amigo y docente digital...Jonathan Mircha')
+// 	e.preventDefault()
+// })
+
+// 75. DOM: Delegacción y eventos
+
+document.addEventListener('click', e => {
+	console.log('Click en', e.target)
+
+	if (e.target.matches('.eventos-flujo div')) {
+		flujoEventos(e)
+	}
+
+	if (e.target.matches('.eventos-flujo a')) {
+		alert('Hola soy tu amigo y docente digital...Jonathan Mircha')
+		e.preventDefault()
+	}
 })
+
+// $divsEventos.forEach(div => {
+// 	div.addEventListener('click', flujoEventos)
+// })
