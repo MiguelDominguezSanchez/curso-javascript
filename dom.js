8b8f1d280180da296657a695c325b9c5b420decb
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-28 17:12:38 +0100

    77. BOM: Propiedades y Eventos

--- dom.js
+++ dom.js
@@ -343,3 +343,41 @@ document.addEventListener('click', e => {
 // $divsEventos.forEach(div => {
 // 	div.addEventListener('click', flujoEventos)
 // })
+
+// 77. BOM: Propiedades y Eventos
+
+window.addEventListener('resize', e => {
+	console.clear()
+	console.log('**********Evento Resize*********')
+	console.log(window.innerWidth)
+	console.log(window.innerHeight)
+	console.log(window.outerWidth)
+	console.log(window.outerHeight)
+	console.log(e)
+})
+
+window.addEventListener('scroll', e => {
+	console.clear()
+	console.log('*********Evento Scroll*************')
+	console.log(window.scrollX)
+	console.log(window.scrollY)
+	console.log(e)
+})
+
+window.addEventListener('load', e => {
+	console.log('*********Evento Load*************')
+	console.log(window.screenX)
+	console.log(window.screenY)
+	console.log(e)
+})
+
+// 4(window).load()
+// $(document).ready(funcion)
+// $(window).on('load', funcion)
+
+document.addEventListener('DOMContentLoaded', e => {
+	console.log('*********Evento DOMContentLoaded*************')
+	console.log(window.screenX)
+	console.log(window.screenY)
+	console.log(e)
+})
