652950b4a373943dd1dd4c807de8393020922fc2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-02 09:54:29 +0100

    86. DOM: Ejercicios Prrácticos | Cuenta Regrresiva (countdown)

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,3 +1,4 @@
+import countdown from './dom/cuenta_regresiva.js'
 import hamburgerMenu from './dom/menu_hamburguesa.js'
 import { digitalClock, alarm } from './dom/reloj.js'
 import { moveBall, shortcuts } from './dom/teclado.js'
@@ -8,6 +9,11 @@ d.addEventListener('DOMContentLoaded', e => {
 	hamburgerMenu('.panel-btn', '.panel', '.menu a')
 	digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj')
 	alarm('assets/alarma.mp3', '#activar-alarma', '#desactivar-alarma')
+	countdown(
+		'countdown',
+		'Mar 02, 2021 09:52:00',
+		'Feliz Cumpleaños amigo y docente digital'
+	)
 })
 
 d.addEventListener('keydown', e => {
